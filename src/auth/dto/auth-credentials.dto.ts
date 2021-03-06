ee1e91c3dd1fd6d693d00d054cdd80ed3ded1153
Kushan Shamika <shamikakushan@gmail.com>
2021-03-06 16:11:10 +0530

    hash user password

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,12 @@
+import { IsEmail, Matches } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsEmail()
   username: string;
+
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    { message: 'password too week' },
+  )
   password: string;
 }
