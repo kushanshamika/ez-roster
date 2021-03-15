72e8adfe7447e2f381645f343ab36dcc96e61aa7
Kushan Shamika <shamikakushan@gmail.com>
2021-03-15 21:47:10 +0530

    auth/me

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,33 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { GetUser } from './get-user.decorator';
+import { User } from '../user/user.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/login')
-  signIn(
+  login(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
     return this.authService.login(authCredentialsDto);
+  }
+
+  @Get('/me')
+  @UseGuards(AuthGuard())
+  loggedInUser(@GetUser() user: User): User {
+    user.password = undefined;
+    user.salt = undefined;
+    return user;
   }
 }
